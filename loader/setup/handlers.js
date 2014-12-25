208a0ec045609665a0a38d4d919e8b9614dd6832
uzquiano <mgu1@cornell.edu>
2014-12-24 20:39:04 -0500

    Updated SDK with latest sample site content + web site

--- loader/setup/handlers.js
+++ loader/setup/handlers.js
@@ -4,17 +4,8 @@ module.exports = function()
 
     r.uninstalls = function(installer)
     {
-        /*
-        // product catalog (cupcakes)
-        installer.removeContent("catalog:product");
-        installer.removeContent("catalog:article");
-        installer.removeContentAtPath("/Samples/Products");
-        */
-
-        // remove wcm things
+        // clean things up
         installer.removeContentInstances("wcm:page");
-        installer.removeContentInstances("wcm:template");
-
         installer.removeContentPackage("_common");
         installer.removeContentPackage("catalog");
         installer.removeContentPackage("guide");
@@ -22,32 +13,6 @@ module.exports = function()
 
     r.installs = function(installer)
     {
-        /*
-        // product catalog (cupcakes)
-        installer.addContent({
-            "title": "Catalog Product",
-            "_qname": "catalog:product",
-            "_type": "d:type"
-        });
-        installer.addContent({
-            "title": "Catalog Article",
-            "_qname": "catalog:article",
-            "_type": "d:type"
-        });
-        installer.addContent({
-            "_type": "catalog:product",
-            "title": "Cupcake #1"
-        }, {
-            "parentFolderPath": "/Samples/Catalog/Products"
-        });
-        installer.addContent({
-            "_type": "catalog:article",
-            "title": "Article #1"
-        }, {
-            "parentFolderPath": "/Samples/Catalog/Articles"
-        });
-        */
-
         installer.addContentPackage("_common");
         installer.addContentPackage("catalog");
         installer.addContentPackage("guide");
