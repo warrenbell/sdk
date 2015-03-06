8f484d11d7d22f1ad98b5eb67f2862e1180f4680
Andrew Hunt <andrew@hunt.li>
2015-03-06 08:36:07 -0500

    Adding more comments.  Small changes in anticipation of a more generic approach

--- AngularJS/hello-angular-browser/public/app/controllers.js
+++ AngularJS/hello-angular-browser/public/app/controllers.js
@@ -5,7 +5,7 @@ var helloCloudcms = angular.module("helloCloudcms", []);
 var gitanaConnected;
 
 // connect to cloudCMS.  CONNECTION_CREDENTIALS is a global stored to
-// separate credentials from this file. For demo purposes only.
+// separate credentials from this file (./public/GITANA_CREDENTIALS.js). For demo purposes only.
 // You should not make credentials available to the browser directly
 // in production.
 gitanaConnected = Gitana.connect(CONNECTION_CREDENTIALS);
@@ -14,41 +14,38 @@ helloCloudcms.controller("StoreCtrl", function ($scope) {
     // a reference to the scope of the controller for use in callbacks below.
     // (alternatively we could use $scope.)
     var self = this;
-
+    var query = {"_type":"catalog:product"};
     /**
-     *
+     * a collection of nodes that we will load from the server and show
      */
-    this.cupcakes = [];
+    this.nodes = [];
 
     // using the promise we created above, we can chain requests to CloudCMS
     // that should happen asynchronously -- after authentication has (hopefully) succeeded.
     gitanaConnected.then(function () {
-        this.readRepository('af4bdc9c1daa4026ff02')
-            .readBranch('master')
-            .queryNodes({
-                "_type":"catalog:product"
-                //"_type": {
-                    //"$regex":".*catalog"
-                //}
-        }, function (err) {
+        // REPOSITORY is a global stored in ./public/GITANA_CREDENTIALS.js.  Add a reference to your repository.
+        // TODO: change this to a string reference?  e.g. 'content' like datastore('content') or something similar?
+        this.readRepository(REPOSITORY)
+            .readBranch("master")
+            .queryNodes(query).then(function () {/*no-op.  Nothing to intercept if we succeed.*/},
+               function (err) {
             // when the previous promise results in an error,
             // notify the user through a notification.
+            // XXX this doesn't seem to work.
             self.notification = "Could not connect to CloudCMS: " + err.message;
             $scope.$apply();
         }).each(function () {
-            self.cupcakes.push({
-                "name":this.get('title'),
-                "id":this.get('id'),
-                "headline":this.get('headline'),
+            // TODO refactor the building of this object to build
+            // its exposed nodes through reflection.
+            self.nodes.push({
+                "name":this.get("title"),
+                "id":this.get("id"),
+                "headline":this.get("headline"),
                 "uri": this.getUri(),
-                "image":{
-                    "icon_64_64":"_preview_default_icon64_64",
-                    "default":"default",
-                    "_preview_default_upload64_64":""
-                },
-                "summary":this.get('summary')
+                "summary":this.get("summary")
             });
         }).then(function () {
+            // clear the loading notification
             self.notification = "";
             $scope.$apply();
         });
@@ -56,17 +53,16 @@ helloCloudcms.controller("StoreCtrl", function ($scope) {
     /**
      * a string that will be written to the UI to
      * indicate when things are going well, or poorly.
+     *
      */
-    this.notification = "Loading cupcakes...";
+    this.notification = "Loading " + query._type + "...";
 
     /**
-     *
+     * build and return a uri referencing the default attachment image
+     * of the given node.
+     * @return String uri
      */
-    this.getImageUri = function (cupcake) {
-        return "/proxy" +  cupcake.uri + "/attachments/default";
+    this.getImageUri = function (node) {
+        return "/proxy" +  node.uri + "/attachments/default";
     };
-    /**
-     *
-     */
-    this.defaultImageUri = "default";
 });
